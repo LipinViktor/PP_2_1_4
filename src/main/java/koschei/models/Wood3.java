69aeeb4b47e7655a23be096af79b53c1f53df6e9
Lipin Viktor <lipivv1986@gmail.com>
2021-11-28 09:22:50 +1000

    Предложение собрано =), тест пройден!

--- src/main/java/koschei/models/Wood3.java
+++ src/main/java/koschei/models/Wood3.java
@@ -6,12 +6,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class Wood3 {
 
+
     private final Rabbit4 rabbit;
 
     @Autowired
     public Wood3(Rabbit4 rabbit) {
         this.rabbit = rabbit;
     }
+
 
     @Override
     public String toString() {
